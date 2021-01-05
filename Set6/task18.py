a877c7fa2b9b23361f7d781ff99e5fa5e18f2eba
margolek <margolek555@gmail.com>
2021-01-05 18:06:57 +0100

    Create task18.py

---
+++ Set6/task18.py
@@ -0,0 +1,26 @@
+"""
+A website requires the users to input username and password to register.
+Write a program to check the validity of password input by users.
+
+Following are the criteria for checking the password:
+
+At least 1 letter between [a-z]
+At least 1 number between [0-9]
+At least 1 letter between [A-Z]
+At least 1 character from [$#@]
+Minimum length of transaction password: 6
+Maximum length of transaction password: 12
+
+Your program should accept a string passwords and
+will check them according to the above criteria. Passwords that match
+the criteria are to be printed, each separated by a comma.
+
+input:
+ABd1234@1,a F1#,2w3E*,2We3345
+
+output:
+ABd1234@1
+None
+None
+
+
